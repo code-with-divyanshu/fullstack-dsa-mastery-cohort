3a711fe473c92e1d11ecdd7f530ed0e5025e1620
code-with-divyanshu <divyanshubartwal67@gmail.com>
2025-08-04 00:41:02 +0530

    Day_16 Create Login Api and Define Controller in Controller Folder

--- Backend/social-media-project/backend/src/routes/auth.routes.js
+++ Backend/social-media-project/backend/src/routes/auth.routes.js
@@ -1,39 +1,15 @@
 const express = require("express");
-const userModel = require("../models/user.model");
-const jwt = require("jsonwebtoken");
+const {
+  registerController,
+  loginController,
+} = require("../controllers/auth.controller");
 
 const router = express.Router();
 
 // register api
+router.post("/register", registerController);
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-
-  const existingUser = await userModel.findOne({
-    username,
-  });
-
-  if (existingUser) {
-    return res.status(409).json({
-      message: "Username already in use",
-    });
-  }
-
-  const user = await userModel.create({
-    username,
-    password,
-  });
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-
-  res.cookie("accessToken", token, {
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
-  });
-
-  res.status(201).json({
-    message: "User Created Succesfully",
-    user,
-  });
-});
+//  login api
+router.post("/login", loginController);
 
 module.exports = router;
