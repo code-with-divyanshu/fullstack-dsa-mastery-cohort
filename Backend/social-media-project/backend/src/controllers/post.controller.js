af936997855f62388192a1b8ec9543dcdc9a9931
code-with-divyanshu <divyanshubartwal67@gmail.com>
2025-08-05 02:06:16 +0530

    Complete Post Route Save Image Url from Imagekit and Caption Generated From AI in DB

--- Backend/social-media-project/backend/src/controllers/post.controller.js
+++ Backend/social-media-project/backend/src/controllers/post.controller.js
@@ -1,5 +1,29 @@
+const postModel = require("../models/post.model");
+const generateCaption = require("../services/ai.service");
+const uploadFile = require("../services/storage.service");
+const { v4: uuidv4 } = require("uuid");
+
 const createPostController = async (req, res) => {
   const file = req.file;
+
+  const base64Image = Buffer.from(file.buffer).toString("base64");
+
+  const caption = await generateCaption(base64Image);
+
+  const result = await uploadFile(file.buffer, `${uuidv4()}`);
+
+  const post = await postModel.create({
+    image: result.url,
+    caption: caption,
+    user: req.user._id,
+  });
+
+  res.status(201).json({
+    message: "Post Created Successfully",
+    post,
+  });
 };
 
-module.exports = createPostController;
+module.exports = {
+  createPostController,
+};
